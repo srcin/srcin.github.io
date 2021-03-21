af1bb97aae8e15203245b42e87366a0bfee04ce4
ArcherBle <alzioarq@icloud.com>
2021-03-21 20:09:21 +0800

    Update 2021/3/21 second update

--- js/searchchin.js
+++ js/searchchin.js
@@ -2,16 +2,19 @@
 The search.js is SearchIndex's core search 
 function.Chinese version.
 */
+
 function search(){
 	var searchtext = document.getElementById("searchinput").value
+     
+    document.getElementById("searchbuttoncn").disabled=false;
 
    if (searchtext == "") {
       console.log("No input.")
    } else { 
        
-    window.open("https://cn.bing.com/search?q="+searchtext+"&FORM=BEHPTB&ensearch=0")
+    window.open("https://cn.bing.com/search?q="+searchtext+"&FORM=BESBTB&ensearch=0")
    
-    console.log("Search Url:https://cn.bing.com/search?q="+searchtext+"&FORM=BEHPTB&ensearch=0")
+    console.log("Search Url:https://cn.bing.com/search?q="+searchtext+"&FORM=BESBTB&ensearch=0")
 	
     console.log(console.error())
 
